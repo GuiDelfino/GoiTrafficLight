cb9171517f5ea067aa790ad654c49805dcaa16d9
Sarah Rodrigues <msrodrigues.sarah@gmail.com>
2023-12-19 08:23:26 -0300

    Initial Commit

---
+++ GoiTrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoiTrafficLight
+//
+//  Created by Sarah Rodrigues on 19/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
